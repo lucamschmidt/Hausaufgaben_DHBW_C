9e57c7287f5087102722b52e9904e5bcb7b790be
jannikwiessler <56755425+jannikwiessler@users.noreply.github.com>
2020-06-05 18:33:48 +0200

    0/10

--- Hausaufgabe_3+4/HA03_linkedListLib.c
+++ Hausaufgabe_3+4/HA03_linkedListLib.c
@@ -316,7 +316,13 @@ JW-2020-05-27:
 > well done (10/10) 
 > how much time did you spend on this exercise? I think it was a litte to easy wasnt it?
 */
-
+/*
+JW-2020-06-05:
+--------------
+> well done (0/10) 
+> Sorting does not work at all !
+> Only switching first char in last name ???
+*/
 
 
 
